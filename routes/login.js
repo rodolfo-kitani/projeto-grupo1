b08ff35253808d4b653db0cf855155fdb15f86fd
Moisés Ximenes <moises.ximenes5@gmail.com>
2021-03-25 19:02:10 -0300

    adicionando o return n route/login

--- routes/login.js
+++ routes/login.js
@@ -4,15 +4,16 @@ const router = express.Router();
 
 const model = require('../models/login');
 
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
     res.render('login')
 });
 
 router.post('/', function(req, res) {
-    let {email, password} = req.body
+    let { email, password } = req.body
     let user = model.authenticator(email, password)
     if (user === undefined) {
         res.redirect('/login');
+        return;
     }
     req.session.user = user;
     res.redirect('/novo-produto');
