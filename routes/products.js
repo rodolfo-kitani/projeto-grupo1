9030bb328f87f1109f8bc053c0325bcd5f6815f1
Moisés Ximenes <moises.ximenes5@gmail.com>
2021-03-30 19:34:11 -0300

    adicionando méthodo DELETE

--- routes/products.js
+++ routes/products.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 // Importação dos  Middleware
 const newProductMiddleware = require('../middlewares/newProduct');
-const newProductControllers = require('../controllers/newProductController')
+const newProductControllers = require('../controllers/newProductController');
+const { getProducts } = require('../models/products');
 
 const router = express.Router();
 
@@ -13,37 +14,46 @@ const router = express.Router();
 
 //Multer recebe o upload de img
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join('public','images','uploads'))
-  },
-  filename: function (req, file, cb) {
-    cb(null, /*file.fieldname + '-' + Date.now()*/ file.originalname)
-  }
+    destination: function(req, file, cb) {
+        cb(null, path.join('public', 'images', 'uploads'))
+    },
+    filename: function(req, file, cb) {
+        cb(null, /*file.fieldname + '-' + Date.now()*/ file.originalname)
+    }
 })
 
 const upload = multer({ storage: storage })
+
+let productsEdit = getProducts();
 
 // http://localhost:3000/products
 
 /* GET Listagem de Produto */
 router.get('/', function(req, res, next) {
-  res.render('products', { title: 'produtos', products: products.getProducts() });
+    res.render('products', { title: 'produtos', products: products.getProducts() });
 });
 
 
 /* GET do Cadastro de Produto */
-router.get('/create', 
-    newProductMiddleware.validateUser, 
+router.get('/create',
+    newProductMiddleware.validateUser,
     newProductControllers.get
 );
 
 /* POST do Cadastro de Produto */
 router.post('/create',
-  upload.single("photo"),
-  newProductMiddleware.validateUser,
-  newProductMiddleware.validateInput,
-  newProductControllers.post
+    upload.single("photo"),
+    newProductMiddleware.validateUser,
+    newProductMiddleware.validateInput,
+    newProductControllers.post
 );
 
+router.delete('/create/:id', function(req, res) {
 
+
+    products = products.filter(function(product) {
+        return product.id !== parseInt(req.params.id);
+    })
+    res.render('products', { products: products })
+});
 module.exports = router;
